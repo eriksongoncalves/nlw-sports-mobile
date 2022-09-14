a5920fcdd5d2d23f907cee2b25c7a93b7688b024
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-13 22:41:15 -0300

    feat: loading fonts

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+
+import { styles } from './styles'
+
+// type HomeProps = {
+
+// }
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
