80440beb6e2d019855d8120b30f0bb0db31ebf7b
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2022-09-17 19:19:34 -0300

    feat: add push notification

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import {
   useFonts,
@@ -6,10 +7,16 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core'
+import * as Notifications from 'expo-notifications'
 
 import { Background } from './src/components/Background'
 import { Loading } from './src/components/Loading'
 import { Routes } from './src/routes'
+
+import './src/service/notificationConfigs'
+import { getPushNotificationToken } from './src/service/getPushNotificationToken'
+import { removeNotificationSubscription } from 'expo-notifications'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +25,34 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black
   })
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, [])
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener(notification => {
+        console.log('>>> ReceivedListener ', notification)
+      })
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener(response => {
+        console.log('>>> ResponseReceivedListener', response)
+      })
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        removeNotificationSubscription(getNotificationListener.current)
+        removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  }, [])
 
   return (
     <Background>
